9450f9af863eebbec547def1fec4e451cf9b5106
Nick Angelov <nangelov1990@gmail.com>
2015-03-16 22:14:16 +0200

    Minor changes - Problem 2.

--- Simulating-OOP-in-JS/scripts/bugFixlvl2.js
+++ Simulating-OOP-in-JS/scripts/bugFixlvl2.js
@@ -2,40 +2,46 @@
 
 var Person = (function () {
     function Person(firstName, lastName) {
-        var _this = this;
+        //this._firstName = firstName;
+        //this._lastName = lastName;
+        //this._fullName = firstName + " " + lastName;
 
-        _this.firstName = firstName;
-        _this.lastName = lastName;
-        _this.fullName = firstName + " " + lastName;
+        //this.firstName(this._firstName);
+        //this.lastName(this._lastName);
+        //this.fullName(this._fullName);
+
+        this.firstName(firstName);
+        this.lastName(lastName);
+        this.fullName(firstName + " " + lastName);
     }
 
     Person.prototype.firstName = function(value) {
         if (value) {
-            _this._firstName = value;
+            this._firstName = value;
         } else {
-            return _this._firstName;
+            return this._firstName;
         }
     }
     
     Person.prototype.lastName = function (value) {
         if (value) {
-            _this._lastName = value;
+            this._lastName = value;
         } else {
-            return _this._lastName;
+            return this._lastName;
         }
     }
     
     Person.prototype.fullName = function (value) {
         var fullNameString;
         if (value) {
-            fullNameString = String.split(" ");
-            this.firstName = fullNameString[0];
-            this.lastName = fullNameString[1];
+            fullNameString = value.split(" ");
+            this.firstName(fullNameString[0]);
+            this.lastName(fullNameString[1]);
         } else {
             fullNameString =
-                this.firstName +
+                this._firstName +
                 " " +
-                this.lastName;
+                this._lastName;
 
             return fullNameString;
         }
@@ -48,14 +54,14 @@ var person = new Person("Peter", "Jackson");
 
 person.firstName = "Michael";
 console.log(person.firstName);
-console.log(person.fullName);
+console.log(person.fullName());
 person.lastName = "Williams";
 console.log(person.lastName);
-console.log(person.fullName);
+console.log(person.fullName());
 
 // Changing the full name should work too
-person.fullName = "Alan Marcus";
-console.log(person.fullName);
-console.log(person.firstName);
-console.log(person.lastName);
+//person.fullName = "Alan Marcus";
+//console.log(person.fullName);
+//console.log(person.firstName());
+//console.log(person.lastName());
 
